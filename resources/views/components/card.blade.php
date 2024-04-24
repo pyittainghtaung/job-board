90a1908249197f1a328ad09ac0f2e6f058ccd05f
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-04-24 13:11:17 +0630

    Jobs Page Tag Component & Job Info

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -5,6 +5,7 @@ First Style --}}
 </div> --}}
 
 {{-- This is second Style --}}
-<div {{ $attributes->class(['rounded-md border border-slate-300 bg-white p-4 shadow-sm']) }}>
+<article
+     {{ $attributes->class(['rounded-md border border-slate-300 bg-white p-4 shadow-sm']) }}>
     {{ $slot }}
-</div>
+</article>
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -11,7 +11,28 @@
         </div> --}}
 
         <x-card class="mb-4">
-            {{ $job->title }}
+            <div class="flex justify-between border mb-4">
+                <h2 class="text-lg font-medium">
+                    {{ $job->title }}
+                </h2>
+                <div class="text-slate-500">
+                    ${{ number_format($job->salary) }}
+                </div>
+            </div>
+            <div class="mb-4 flex justify-between text-sm text-slate-500 items-center border">
+                <div class="flex space-x-4">
+                    <div>Company Name</div>
+                    <div>{{ $job->location }}</div>
+                </div>
+
+                <div class="flex space-x-1 text-xs">
+                    <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
+                    <x-tag>{{ $job->category }}</x-tag>
+                </div>
+            </div>
+            <p class="text-sm text-slate-500">
+                {!! nl2br(e($job->description)) !!}
+            </p>
         </x-card>
 
     @empty
