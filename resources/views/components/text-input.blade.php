41cc26910936d8d3a387f3c0429a076ab0bdb1fc
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-04-30 14:46:20 +0630

    Filtering Jobs Tailwind Form Plugin & Text Inputs

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,5 @@
+{{-- <div>
+    <!-- It is never too late to be what you might have been. - George Eliot -->
+</div> --}}
+<input type="text" placeholder="{{ $placeholder }}" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
+    class="w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,5 +1,23 @@
 <x-layout>
-    <x-breadcrumbs class="mb-4" :links="['Jobs'=>route('jobs.index')]"/>
+    <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>
+                <div class="mb-1 font-semibold">Search</div>
+                <x-text-input name="search" value="" placeholder="Search for any text" />
+                {{-- if name, value and placeholder is dynamic field, we will use :name="{{ $job->name }}", :value="{{ $job->value }}" --}}
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Salary</div>
+                <div class="flex space-x-2">
+                    <x-text-input name="min_salary" value="" placeholder="From" />
+                    <x-text-input name="max_salary" value="" placeholder="To" />
+                </div>
+            </div>
+            <div>3</div>
+            <div>4</div>
+        </div>
+    </x-card>
     @forelse ($jobs as $job)
         {{-- First Style .//This div will be changed to be a card.This is a first code
         <div>
