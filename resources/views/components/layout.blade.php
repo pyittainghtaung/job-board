3107cd5fb1f8d9754a67e1a30330d1f0c9ece2b4
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-05-07 16:22:25 +0630

    Refactor Time Gradient Background, Styling Buttons, Adding Alpine.js Part 2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel Job Board</title>
-    @vite('resources/css/app.css')
+
+    {{-- we can write these two lines to one single line --}}
+    {{-- @vite('resources/css/app.css')
+    @vite('resources/js/app.js') --}}
+
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 
 <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90% text-slate-700">
