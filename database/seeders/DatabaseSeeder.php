8806329aad240500ab7719316686d2446526e3f9
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-05-10 15:52:08 +0630

    Employer Factory & Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,33 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        \App\Models\Job::factory(100)->create();
+        \App\Models\User::factory(300)->create();
+
+        // Shuffling user list with shuffle method
+        $users = \App\Models\User::all()->shuffle();
+
+        for ($i = 0; $i < 20; $i++) {
+            // pop() method work like that
+            // $collection = collect([1, 2, 3, 4, 5]);
+
+            // Remove and retrieve the last item
+            // $lastItem = $collection->pop(); // $lastItem = 5
+
+            // Collection now contains [1, 2, 3, 4]
+            // ************************************************************************
+            // Pop() method select an item from list and remove that item from the list
+            \App\Models\Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+        $employers = \App\Models\Employer::all();
+        for ($i = 0; $i < 100; $i++) {
+            \App\Models\Job::factory()->create([
+                'employer_id'=>$employers->random()->id
+            ]);
+        }
+        // \App\Models\Job::factory(100)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
