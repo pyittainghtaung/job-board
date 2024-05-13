35b8b12174365e9b98b0270d62c25565ddcd8711
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-05-13 16:54:52 +0630

    Authentication Sign-in Form

--- app/View/Components/TextInput.php
+++ app/View/Components/TextInput.php
@@ -12,12 +12,12 @@ class TextInput extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public ?string $value=null,
-        public ?string $name=null,
-        public ?string $placeholder=null,
-        public ?string $formRef=null
-    )
-    {
+        public ?string $value = null,
+        public ?string $name = null,
+        public ?string $placeholder = null,
+        public ?string $formRef = null,
+        public ?string $type = 'text'
+    ) {
         //
     }
 
