8f4e685b208e26f1b8a05ac6eef849f63e4fbc0f
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-06-06 11:58:18 +0630

    My Applications Applicant Count, Average Asking Salary

--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -1,11 +1,31 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" :links="['My Job Application' => '#']" />
 
-        @forelse ($applications as $application )
-            <x-job-card :job="$application->job">
-
-            </x-job-card>
-        @empty
-            There is no Job Application
-        @endforelse
+    @forelse ($applications as $application)
+        <x-job-card :job="$application->job">
+            <div class="flex items-center justify-between text-xs text-slate-500">
+                <div>
+                    <div>
+                        Applied {{ $application->created_at->diffForHumans() }}
+                    </div>
+                    <div>
+                        Other {{ Str::plural('applicant', $application->job->job_applications_count - 1) }}
+                        {{ $application->job->job_applications_count - 1 }}
+                    </div>
+                    <div>
+                        Your asking salary ${{ number_format($application->expected_salary) }}
+                    </div>
+                    <div>
+                        Average asking salary
+                        ${{ number_format($application->job->job_applications_avg_expected_salary) }}
+                    </div>
+                </div>
+                <div>
+                    Right
+                </div>
+            </div>
+        </x-job-card>
+    @empty
+        There is no Job Application
+    @endforelse
 </x-layout>
