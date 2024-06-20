b06bfb9e7847e1f1f37b2b04b2dda3a4e22a0314
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-06-20 21:32:10 +0630

    Refactor Time Label Component

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -6,11 +6,11 @@
         <form action="{{ route('auth.store') }}" method="POST">
             @csrf
             <div class="mb-8">
-                <label for="email" class="mb-2 block text-sm font-medium text-slate-900">E-Mail</label>
+                <x-label for="email" :required="true">E-Mail</x-label>
                 <x-text-input name="email" />
             </div>
             <div class="mb-8">
-                <label for="password" class="mb-2 block text-sm font-medium text-slate-900">Password</label>
+                <x-label for="password" :required="true">Password</x-label>
                 <x-text-input name="password" type="password" />
             </div>
             <div class="mb-8 flex justify-between text-sm font-medium">
