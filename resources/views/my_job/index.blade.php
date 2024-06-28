cb56f131eede5e66d7874076534251c1c4efa562
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-06-28 21:04:51 +0630

    Employer Job List-1

--- resources/views/my_job/index.blade.php
+++ resources/views/my_job/index.blade.php
@@ -1,5 +1,24 @@
 <x-layout>
-    <x-card>
-        List of All Jobs
-    </x-card>
+    <x-breadcrumbs :links="['My Jobs' => '#']" class="mb-4" />
+    <div class="mb-8 text-right">
+        <x-link-button href="{{ route('my-jobs.create') }}">Add New</x-link-button>
+    </div>
+    @forelse ($jobs as $job)
+        <x-job-card :$job>
+            <div class="text-xs text-slate-500">
+                @forelse ($job->jobApplications as $application)
+                    <div class="mb-4 flex items-center justify-between">
+                        <div>
+                            <div>
+                        </div>
+                        <div>Right</div>
+                    </div>
+                @empty
+                    <div>No Applications Yet</div>
+                @endforelse
+            </div>
+        </x-job-card>
+    @empty
+        No Jobs
+    @endforelse
 </x-layout>
