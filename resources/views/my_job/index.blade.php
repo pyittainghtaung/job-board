f620754b03929ed566117123fe209eef9c6307a3
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-06-30 10:28:20 +0630

    Update index.blade.php

--- resources/views/my_job/index.blade.php
+++ resources/views/my_job/index.blade.php
@@ -9,9 +9,11 @@
                 @forelse ($job->jobApplications as $application)
                     <div class="mb-4 flex items-center justify-between">
                         <div>
-                            <div>
+                            <div>{{ $application->user->name }}</div>
+                            <div>Applied {{ $application->created_at->diffForHumans() }}</div>
+                            <div>Download CV</div>
                         </div>
-                        <div>Right</div>
+                        <div>${{ number_format($application->expected_salary) }}</div>
                     </div>
                 @empty
                     <div>No Applications Yet</div>
@@ -19,6 +21,14 @@
             </div>
         </x-job-card>
     @empty
-        No Jobs
+        <div class="rounded-md border border-dashed border-slate-300 p-8">
+            <div class="text-center font-medium">
+                No jobs yet
+            </div>
+            <div class="text-center">
+                Post your first job <a class="text-indigo-500 hover:underline"
+                    href="{{ route('my-jobs.create') }}">here!</a>
+            </div>
+        </div>
     @endforelse
 </x-layout>
